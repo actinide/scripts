b5109623bdc4875dce1e6046b48fe48796eb313a
Heather <heather.huggins@hhuggins3355.machinima.local>
2015-03-18 14:58:06 -0700

    Added some Atom packages

--- macpack.sh
+++ macpack.sh
@@ -8,6 +8,12 @@ if test ! $(which_brew); then
   brew update
 fi
 
+# Install wget
+if test ! $(which_wget); then
+  echo $BLUE'==>'$RESET "Installing wget..."
+  brew install wget
+fi
+
 # Install Packer
 if test ! $(which_packer); then
   echo $BLUE'==>'$RESET "Installing Packer..."
@@ -27,4 +33,17 @@ brew install python
 # Install AWS command line tools
 if test ! $(which_aws); then
   echo $BLUE'==>'$RESET "Installing AWS command line tools..."
+  pip install awscli
+fi
+
+# Install & configure Atom
+if test ! $(which_atom); then
+  echo $BLUE'==>'$RESET "Installing Atom..."
+  wget https://github.com/atom/atom/releases/download/v0.187.0/atom-mac.zip
+  unzip atom-mac.zip
+  mv Atom.app/ /Applications/
+  rm atom-mac.zip
+  echo "Downloading Atom packages..."
+  apm install git-projects
+  apm install git-plus
 fi
