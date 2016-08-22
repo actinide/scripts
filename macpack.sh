d5570efa244ce52bbe41a5f2e865ebde4aab085e
Actinide <actinide@gmail.com>
2016-08-22 16:56:51 -0700

    Added all the things

--- macpack.sh
+++ macpack.sh
@@ -14,6 +14,44 @@ if test ! $(which_wget); then
   brew install wget
 fi
 
+# Install & configure Atom
+if test ! $(which_atom); then
+  echo $BLUE'==>'$RESET "Installing Atom..."
+  wget https://github.com/atom/atom/releases/download/v0.187.0/atom-mac.zip
+  unzip atom-mac.zip
+  mv Atom.app/ /Applications/
+  rm atom-mac.zip
+  echo "Downloading Atom packages..."
+  apm install git-projects
+  apm install git-plus
+  apm install linter
+  apm install linter-ruby
+  apm install linter-python-pep8
+  apm update
+  apm upgrade
+fi
+
+# Install iTerm2
+brew install Caskroom/cask/iterm2
+
+# Install git
+if test ! $(which_git); then
+  echo $BLUE'==>'$RESET "Installing git..."
+  brew install git
+fi
+
+# Install Python
+if test ! $(which_python); then
+  echo $BLUE'==>'$RESET "Installing Python..."
+  brew install python
+fi
+
+# Install Ruby
+if test ! $(which_ruby); then
+  echo $BLUE'==>'$RESET "Installing Ruby..."
+  brew install ruby
+fi
+
 # Install Packer
 if test ! $(which_packer); then
   echo $BLUE'==>'$RESET "Installing Packer..."
@@ -27,23 +65,10 @@ if test ! $(which_ansible); then
   brew install ansible
 fi
 
-# Install and/or update Python
-brew install python
-
 # Install AWS command line tools
 if test ! $(which_aws); then
   echo $BLUE'==>'$RESET "Installing AWS command line tools..."
   pip install awscli
 fi
 
-# Install & configure Atom
-if test ! $(which_atom); then
-  echo $BLUE'==>'$RESET "Installing Atom..."
-  wget https://github.com/atom/atom/releases/download/v0.187.0/atom-mac.zip
-  unzip atom-mac.zip
-  mv Atom.app/ /Applications/
-  rm atom-mac.zip
-  echo "Downloading Atom packages..."
-  apm install git-projects
-  apm install git-plus
-fi
+
